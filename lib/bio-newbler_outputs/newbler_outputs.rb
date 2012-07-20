4484399a6892ae5494e796e9c100442297177885
Ben J. Woodcroft <donttrustben near gmail.com>
2012-07-20 15:39:14 +1000

    added ability to handle 454AlignmentInfo.tsv files, tested

--- lib/bio-newbler_outputs/newbler_outputs.rb
+++ lib/bio-newbler_outputs/newbler_outputs.rb
@@ -1,3 +1,62 @@
-module BioNewblerOutputs
+require 'csv'
 
+module Bio
+  class Newbler
+    class Contig
+      attr_accessor :length, :coverage
+    end
+    
+    
+    # Position Consensus Quality_Score Depth Signal  StdDeviation
+    # >contig00001  1
+    # 1 C 64  6 2.04  0.60
+    # 2 C 20  6 2.04  0.60
+    # 3 G 64  6 1.96  0.55
+    # 4 G 20  6 1.96  0.55
+    class AlignmentInfoFile
+      # Iterate through each row, returning contig_name, columns (string then Array of strings)
+      def self.foreach(filename)
+        current_contig = nil
+        
+        CSV.foreach(filename, :headers => true, :col_sep => "\t") do |row|
+          if matches = row[0].match(/^>(contig\d{5})$/)
+            current_contig = matches[1]
+          else
+            yield current_contig, row
+          end
+        end
+      end
+      
+      # Return a hash of contig name to Contig objects with filled in coverage and length details
+      def self.contig_hash(filename)
+        hash = {}
+        add_contig = lambda do |contig_name, total_count, length|
+          contig = Contig.new
+          contig.length = length
+          contig.coverage = total_count.to_f/length
+          hash[contig_name] = contig
+        end
+        
+        count = 0
+        total_length = 0
+        last_contig = nil
+        foreach(filename) do |contig, cols|
+          last_contig = contig if last_contig.nil?
+          
+          if contig==last_contig 
+            count += cols[3].to_i
+            total_length += 1
+          else
+            add_contig.call(last_contig, count, total_length)
+            count = 0
+            total_length = 0
+          end
+          last_contig = contig
+        end
+        add_contig.call(last_contig, count, total_length)
+        
+        return hash
+      end
+    end
+  end
 end
--- spec/bio-newbler_outputs_spec.rb
+++ spec/bio-newbler_outputs_spec.rb
@@ -1,7 +1,43 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'tempfile'
 
 describe "BioNewblerOutputs" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "should parse a coverage file" do
+    test = "Position        Consensus       Quality Score   Depth   Signal  StdDeviation
+    >contig00001    1
+    1       A       64      13      5.91    0.46
+    2       A       64      13      5.91    0.46
+    3       A       64      13      5.91    0.46
+    4       A       64      13      5.91    0.46
+    5       A       64      13      5.91    0.46
+    6       A       51      13      5.91    0.46
+    7       T       64      13      0.98    0.08
+    8       C       64      13      0.96    0.09
+    9       A       64      13      0.89    0.15
+    10      T       64      13      1.02    0.09
+    11      C       64      14      2.95    0.40
+    12      C       64      14      2.95    0.40
+    13      C       64      14      2.95    0.40
+    14      A       64      14      2.90    0.19
+    >contig00002    1
+    1       T       64      16      0.97    0.04
+    2       G       64      16      1.01    0.05
+    3       C       64      16      0.96    0.04
+    4       G       64      16      2.94    0.27
+    5       G       64      16      2.94    0.27
+    6       G       64      16      2.94    0.27".split("\n").collect{|s| s.strip.gsub(/\s+/,"\t")}.join("\n")
+    
+    Tempfile.open('a') do |tempfile|
+      tempfile.puts test
+      tempfile.close
+      
+      hash = Bio::Newbler::AlignmentInfoFile.contig_hash(tempfile.path)
+      hash.length.should eq(2)
+      hash.to_a[0][0].should eq('contig00001')
+      hash.to_a[1][0].should eq('contig00002')
+      
+      hash.to_a[0][1].coverage.should eq(13.285714285714286)
+      hash.to_a[1][1].coverage.should eq(16.0)
+    end
   end
 end
